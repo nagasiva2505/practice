96ed2dcd0c753a886aeb5c83fea400e8600674ec
nagasiva2505 <83804457+nagasiva2505@users.noreply.github.com>
2024-11-24 02:58:38 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -28,7 +28,7 @@ if is_sendcm_folder:
     while not done:
         page_no += 1
         resp = client.get(url)
-        soup  = BeautifulSoup(resp.content, "lxml")
+        soup = BeautifulSoup(resp.content, "lxml")
         table = soup.find("table", id="xfiles")
         sleep(1)
         files = table.find_all("a", class_="tx-dark")
@@ -39,24 +39,31 @@ if is_sendcm_folder:
             resp2 = client.get(file_url)
             scrape = BeautifulSoup(resp2.text, "html.parser")
             inputs = scrape.find_all("input")
-            file_id = inputs[1]["value"]
-            file_name = findall("URL=(.*?) - ", resp2.text)[0].split("]")[1]
-            parse = {"op": "download2", "id": file_id, "referer": url}
-            sleep(2)
-            resp3 = client.post(base_url, data=parse, allow_redirects=False)
-            dl_url = resp3.headers["Location"]
-            dl_url = dl_url.replace(" ", "%20")
-            print("Fιℓє Nαмє: ", file_name)
-            print("Fɪʟᴇ Lɪɴᴋ: ", file_url)
-            print("Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ: ", dl_url)
-            print("\n")
+
+            # Check if there are enough inputs before accessing them
+            if len(inputs) > 1:
+                file_id = inputs[1]["value"]
+                file_name = findall("URL=(.*?) - ", resp2.text)[0].split("]")[1]
+                parse = {"op": "download2", "id": file_id, "referer": url}
+                sleep(2)
+                resp3 = client.post(base_url, data=parse, allow_redirects=False)
+                dl_url = resp3.headers["Location"]
+                dl_url = dl_url.replace(" ", "%20")
+                print("Fιℓє Nαмє: ", file_name)
+                print("Fɪʟᴇ Lɪɴᴋ: ", file_url)
+                print("Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ: ", dl_url)
+                print("\n")
+            else:
+                print("Error: No file ID found for file: ", file_url)
+                continue  # Skip this file if no file ID is found
+
             pages = soup.find("ul", class_="pagination")
-            if pages == None:
+            if pages is None:
                 done = True
             else:
                 current_page = pages.find("li", "page-item actived", recursive=False)
                 next_page = current_page.next_sibling
-                if next_page == None:
+                if next_page is None:
                     done = True
                 else:
                     url = base_url + next_page["href"]
@@ -64,18 +71,23 @@ else:
     resp = client.get(url)
     scrape = BeautifulSoup(resp.text, "html.parser")
     inputs = scrape.find_all("input")
-    file_id = inputs[1]["value"]
-    file_name = findall("URL=(.*?) - ", resp.text)[0].split("]")[1]
-    parse = {"op": "download2", "id": file_id, "referer": url}
-    sleep(2)
-    resp2 = client.post(base_url, data=parse, allow_redirects=False)
-    dl_url = resp2.headers["Location"]
-    dl_url = dl_url.replace(" ", "%20")
-    print("\n")
-    print("Fιℓє Nαмє: ", file_name)
-    print("Fɪʟᴇ Lɪɴᴋ: ", url)
-    print("Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ: ", dl_url)
-    print("\n")
+
+    # Check if there are enough inputs before accessing them
+    if len(inputs) > 1:
+        file_id = inputs[1]["value"]
+        file_name = findall("URL=(.*?) - ", resp.text)[0].split("]")[1]
+        parse = {"op": "download2", "id": file_id, "referer": url}
+        sleep(2)
+        resp2 = client.post(base_url, data=parse, allow_redirects=False)
+        dl_url = resp2.headers["Location"]
+        dl_url = dl_url.replace(" ", "%20")
+        print("\n")
+        print("Fιℓє Nαмє: ", file_name)
+        print("Fɪʟᴇ Lɪɴᴋ: ", url)
+        print("Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ: ", dl_url)
+        print("\n")
+    else:
+        print("Error: No file ID found for the given URL:", url)
 
 # Download the file using requests
 response = requests.get(dl_url, stream=True)
